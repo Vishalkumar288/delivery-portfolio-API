9f0ee7182be40935bc592eeaa26b534f478def07
VISHAL KUMAR <90972522+Vishalkumar288@users.noreply.github.com>
2026-04-24 08:46:32 +0530

    Comment out ALLOWED_ORIGINS in config.py
    Comment out the ALLOWED_ORIGINS configuration.

--- app/core/config.py
+++ app/core/config.py
@@ -7,5 +7,5 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 SHEET_ID = os.getenv("SHEET_ID") 
 GOOGLE_SERVICE_ACCOUNT = json.loads(os.environ.get("GOOGLE_SERVICE_ACCOUNT"))
-origins_raw = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000")
-ALLOWED_ORIGINS = [origin.strip() for origin in origins_raw.split(",")]
+#origins_raw = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000")
+#ALLOWED_ORIGINS = [origin.strip() for origin in origins_raw.split(",")]
